c9b86447143415216834390184f53b4a0b9be0d2
JackMarx <seriousfools@gmail.com>
2018-10-21 09:22:24 -0500

    multiple pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page. hello thanks for stopping by</h1>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="home">
-    <div v-for="recipe in recipes">
-      {{ recipe }}
+    <div class="row">
+      <div class="col-md-4" v-for="recipe in recipes">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" v-bind:src="recipe.image_url" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">{{ recipe.title }}</h5>
+            <p class="card-text">Prep Time: {{ recipe.formatted.prep_time }}</p>
+            <div class="card-text">
+              <ul>
+                <li v-for="ingredient in recipe.formatted.ingredients">{{ ingredient }}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
